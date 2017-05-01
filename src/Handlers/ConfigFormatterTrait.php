4a03de4d760f9c5f59ff3db723daaf53ca5fda15
Jonathan Robinson <jonrobinson.codes@gmail.com>
2017-05-01 02:03:38 -0500

    add platform validation for valid keys

--- src/Handlers/ConfigFormatterTrait.php
+++ src/Handlers/ConfigFormatterTrait.php
@@ -5,10 +5,13 @@ namespace EventGator\Handlers;
 trait ConfigFormatterTrait
 {
     protected $validPlatforms = array('facebook', 'eventbrite');
+    protected $validFbKeys = ['app_id', 'app_secret', 'default_graph_version', 'graph_node_id'];
+    protected $validEbKeys = ['app_key', 'app_version', 'organizer_id'];
 
     /**
      * @param array $config
      *
+     * @return array
      * @throws \Exception
      */
     public function validateConfig($config)
@@ -17,6 +20,44 @@ trait ConfigFormatterTrait
             if (!in_array($platform, $this->validPlatforms)) {
                 throw new \Exception("Error: Please enter a valid Platform. Platform Entered: ".$platform);
             }
+
+            $this->validatePlatformKeys($platform, $config[$platform]);
         }
+
+        return $config;
+    }
+
+    /**
+     * @param string $platform
+     * @param array  $credentials
+     *
+     * @throws \Exception
+     */
+    protected function validatePlatformKeys($platform, $credentials)
+    {
+        foreach(array_keys($credentials) as $key) {
+            if (!in_array($key, $this->getValidKeysByPlatform($platform))) {
+                throw new \Exception("Error: Please enter a valid Key. Key Entered: ".$key);
+            }
+        }
+    }
+
+    /**
+     * @param $platform
+     *
+     * @return array
+     * @throws \Exception
+     */
+    protected function getValidKeysByPlatform($platform)
+    {
+        switch ($platform) {
+            case 'facebook':
+                return $this->validFbKeys;
+
+            case 'eventbrite':
+                return $this->validEbKeys;
+        }
+
+        throw new \Exception("Missing Valid PLatform. Platform Entered: ".$platform);
     }
 }
